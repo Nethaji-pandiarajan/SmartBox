991eb46332bab8d5bdc1eb76b41eb92e9bef3323
Nethaji-pandiarajan <82449478+Nethaji-pandiarajan@users.noreply.github.com>
2022-07-21 23:04:54 +0530

    Add files via upload

---
+++ src/SceneProperty.java
@@ -0,0 +1,26 @@
+
+public class SceneProperty {
+	private Double sceneWidth;
+	private Double sceneHeight;
+	private String sceneName;
+	
+	public Double getSceneWidth() {
+		return sceneWidth;
+	}
+	public void setSceneWidth(Double sceneWidth) {
+		this.sceneWidth = sceneWidth;
+	}
+	public Double getSceneHeight() {
+		return sceneHeight;
+	}
+	public void setSceneHeight(Double sceneHeight) {
+		this.sceneHeight = sceneHeight;
+	}
+	public String getSceneName() {
+		return sceneName;
+	}
+	public void setSceneName(String sceneName) {
+		this.sceneName = sceneName;
+	}
+
+}
